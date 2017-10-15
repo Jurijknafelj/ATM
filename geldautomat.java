e7897d41046bffdb674039392f462ac4b37e9910
Dominik Knafelj <d.knafelj@gmail.com>
2017-10-15 14:31:28 -0400

    Refactoring ATM

--- geldautomat.java
+++ geldautomat.java
@@ -1,40 +1,84 @@
 import java.util.Scanner;
 public class Geldautomat
 {
+  static int guthaben = 200;
+  static Scanner eingabe = new Scanner(System.in);
+
   public static void main (String [] args)
   {
-    int guthaben = 200, x, y , z , PIN = 0;
-    Scanner eingabe = new Scanner(System.in);
-    System.out.println(" Bitte geben Sie ihren PIN ein");
-    PIN = eingabe.nextInt();
-    if (PIN == 8008)
-    {      
-      System.out.println("Guten Tag! Ihr guthaben betraegt " + guthaben + " Euro");
-             
-        
-        
-        System.out.println("Was moechten Sie tun?");                                
-        System.out.println("Einzahlen 1");
-        System.out.println("Auszahlen 2");  
-        x = eingabe.nextInt();
-        
-        if (x==1)        
-        {
-          System.out.println("Welchen Betrag moechten sie Einzahlen?");
-          y = eingabe.nextInt();
-          guthaben = guthaben + y;
-          System.out.println("Ihr neues Guthaben betr‰gt " + guthaben +" Euro");
-        }
-        if (x==2) 
-        {
-          System.out.println("Welchen Betrag moechten sie Auszahlen?");
-          z = eingabe.nextInt();  
-          guthaben = guthaben - z;
-          System.out.println("Ihr neues Guthaben betr‰gt " + guthaben +" Euro");   
-        }
-        System.out.println("Moechten sie eine weitere Aktion vornehmen? 1");
-        System.out.println("Beenden 2");        
-
+    if(checkPin()){
+      menuEngine();
     }
-  } 
+  }
+
+  private static boolean checkPin(){
+    int pin = 0;
+  
+    System.out.println(" Bitte geben Sie ihren PIN ein");
+    pin = eingabe.nextInt();
+    return pin == 8008;
+  }
+
+  private static void menuEngine(){
+    String x;
+    while(true){
+      printMenu();
+      x = eingabe.next();
+      switch(x) {
+        case "1":
+          einzahlen();
+          break;
+        case "2":
+          auszahlen();
+          break;
+        case "3":
+   
+          return;
+        default:
+      }
+    }   
+  }
+  
+  private static void einzahlen(){
+    String raw_y;
+    int y = 0;
+    System.out.println("Welchen Betrag moechten sie Einzahlen?");
+    raw_y = eingabe.next();
+    if(isInteger(raw_y)){
+      y = Integer.parseInt(raw_y);
+    }
+    else{
+      System.out.println("Falsche Ausgabe.");
+    }
+    guthaben = guthaben + y;
+    System.out.println("Ihr neues Guthaben betr√§gt " + guthaben +" Euro");
+  }
+
+  private static void auszahlen(){
+    int z;
+    System.out.println("Welchen Betrag moechten sie Auszahlen?");
+    z = eingabe.nextInt();  
+    guthaben = guthaben - z;
+    System.out.println("Ihr neues Guthaben betr√§gt " + guthaben +" Euro");   
+  }
+
+  private static void printMenu()
+  {
+      System.out.println("Guten Tag! Ihr guthaben betraegt " + guthaben + " Euro");
+      System.out.println("Was moechten Sie tun?");                                
+      System.out.println("Einzahlen 1");
+      System.out.println("Auszahlen 2");
+      System.out.println("Beenden 3");
+  }
+
+  private static boolean isInteger(String s){
+    try{
+      Integer.parseInt(s);
+      // is an integer!
+      return true;
+    } catch (NumberFormatException e) {
+      // not an integer!
+      return false;
+    }
+  }
 }                                
